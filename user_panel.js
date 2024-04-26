04c2137c1fabd22229ee852e5c3dc1b73efcaedf
Vukan <149599048+Vukotije@users.noreply.github.com>
2024-04-27 01:59:45 +0200

    dynamically loading panel tabel data

---
+++ user_panel.js
@@ -0,0 +1,79 @@
+const fireBaseUrl = "https://wd-sv-67-2023-default-rtdb.firebaseio.com/";
+const usersTableData = document.getElementById("usersTableData");
+
+getUsersTableData();
+
+function getUsersTableData() {
+  let request = new XMLHttpRequest();
+  request.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      if (this.status == 200) {
+        let users = JSON.parse(request.responseText);
+        console.log(users);
+
+        for (let id in users) {
+          let user = users[id];
+          usersTableData.innerHTML += `
+            <tr>
+                <td>${user.korisnickoIme}</td>
+                <td>${user.lozinka}</td>
+                <td>${user.ime}</td>
+                <td>${user.prezime}</td>
+                <td>${user.email}</td>
+                <td>${user.datumRodjenja}</td>
+                <td>${user.adresa}</td>
+                <td>${user.telefon}</td>
+                <td>${user.zanimanje}</td>
+                <td>
+                    <div class="dropdown">
+                        <button
+                        class="btn btn-link dropdown-toggle"
+                        type="button"
+                        id="dropdownMenuButton"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                        >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="1.2em"
+                            fill="dark"
+                            class="bi bi-gear"
+                            viewBox="0 0 16 16"
+                        >
+                            <path
+                            d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492M5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0"
+                            />
+                            <path
+                            d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115z"
+                            />
+                        </svg>
+                        </button>
+                        <ul
+                        class="dropdown-menu p-0"
+                        aria-labelledby="dropdownMenuButton"
+                        >
+                        <li>
+                            <a
+                            class="dropdown-item"
+                            data-bs-toggle="modal"
+                            data-bs-target="#editModal"
+                            href="#"
+                            >Edit</a
+                            >
+                        </li>
+                        <li>
+                            <a class="dropdown-item text-danger" href="#">Delete</a>
+                        </li>
+                        </ul>
+                    </div>
+                </td>
+            </tr>`;
+        }
+      } else {
+        window.location.href = "greska.html?error=" + this.status;
+      }
+    }
+  };
+  request.open("GET", `${fireBaseUrl}/korisnici.json`);
+  request.send();
+}
